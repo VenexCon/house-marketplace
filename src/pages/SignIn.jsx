2d9dfa42eada88e1221d84ca6e2ec47c49243344
VenexCon <venexconsultant@outlook.com>
2022-11-02 22:37:35 +0000

    <Comp Update> sign-in.jsx onSubmit() created & EL attached

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import{getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 
 function SignIn() {
 
@@ -17,12 +18,29 @@ function SignIn() {
   //Able to use links. 
   const navigate = useNavigate()
 
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState, 
       [e.target.id]: e.target.value, 
     }))
   }
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+
+    const userCredential = await signInWithEmailAndPassword(auth, email, password)
+      if(userCredential.user) {
+        console.log()
+        navigate('/')
+      }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
 
 
 
@@ -33,7 +51,7 @@ function SignIn() {
           <p className="pageHeader">Welcome Back!</p>
           </header>
           
-            <form>
+            <form onSubmit={onSubmit}>
               <input type="email" className='emailInput' placeholder='Email Address' id='email' value={email}
               onChange = {onChange} required/>
 
