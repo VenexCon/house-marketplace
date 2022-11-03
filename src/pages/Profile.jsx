9265c17933f6ae3cd393bd2e42c92cd408686792
VenexCon <venexconsultant@outlook.com>
2022-11-03 00:05:10 +0000

    <Comp Update> Profile.jsx, State updates & onChange, no backedn connected yet.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,21 +1,74 @@
 import React from 'react'
-import {getAuth} from 'firebase/auth'
+import {getAuth, updateProfile} from 'firebase/auth'
 import {useEffect, useState} from 'react'
+import {updateDoc} from 'firebase/firestore'
+import {useNavigate, Link} from 'react-router-dom'
+
 
 
 function Profile() {
+  const auth = getAuth()//obtains the current auth object for the firebase db
+  const navigate = useNavigate()
 
-  const [user, setUser] = useState(null)
-  //obtains the current auth object for the firebase db
-  const auth = getAuth()
+  const [changeDetails, setChangeDetails] = useState(false)
 
-  useEffect(()=> {
-    setUser(auth.currentUser)
-  },[])
+  const [formData, setformData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email
+  })
 
+  const {name, email} = formData
 
+  const onLogout = () => {
+    auth.signOut()
+    navigate('/')
+  }
 
-  return user ? <h1>{user.displayName}</h1> : <h1>Not Logged In</h1>
+  const onSubmit = () => {
+    console.log('123')
+  }
+
+  const onChange = (e) => {
+    setformData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value
+    }))
+  }
+  
+  
+  return (
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type='button' className="logOut" onClick={onLogout}>Log Out</button>
+      </header>
+
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p className="changePersonalDetails" onClick={() => {
+            changeDetails && onSubmit()
+            setChangeDetails((prevState) => !prevState)
+          }}>
+            {changeDetails ? 'done' : 'Change'}
+          </p>
+        </div>
+        <div className="profileCard">
+          <form>
+
+            <label htmlFor="name">Name:</label>
+            <input type="text" id= "name" className={!changeDetails ? 'profileName' : 'profileNameActive'}
+            disabled = {!changeDetails} onChange = {onChange} value = {name} />
+
+            <label htmlFor="email">Email:</label>
+            <input type="text" id= "email" className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
+            disabled = {!changeDetails} value = {email} />
+
+          </form>
+        </div>
+      </main>
+    </div>
+  )
 }
 
 export default Profile
