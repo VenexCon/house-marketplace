bd821a79f2a2bc53c2ee0cca887fbd8f5d43cb44
VenexCon <venexconsultant@outlook.com>
2022-11-06 14:45:46 +0000

    <Comp Upd> Updated CreateListing.jsx with image upload and submission to db

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -4,9 +4,13 @@ import {getAuth, onAuthStateChanged } from 'firebase/auth'
 import {useNavigate} from 'react-router-dom'
 import Spinner from '../components/Spinner'
 import {toast} from 'react-toastify'
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import {v4 as uuidv4} from 'uuid'
+import {addDoc, collection, serverTimestamp} from 'firebase/firestore'
+
 
 function CreateListing() {
-    const [geolocationEnabled, setGeolocationEnabled] = useState(true)
+    const [geolocationEnabled, setGeolocationEnabled] = useState(true) //unused due to google billing account
     const [loading, setLoading] = useState(false)
     const [formData, setFormData] = useState({
         type: 'rent', 
@@ -26,7 +30,8 @@ function CreateListing() {
 
     const {type, name, bathrooms, bedrooms, parking, furnished, address, 
         offer, regularPrice,discountedPrice, images, lat, long} = formData
-
+    
+   
     const auth = getAuth()
     const navigate = useNavigate()
     const isMounted = useRef(true)
@@ -83,8 +88,72 @@ function CreateListing() {
            location = address
         }
 
+        setLoading(false)
 
+      //store images in firebase
+      //takes into account a single image
+      const storeImage = async (image) => {
+        return new Promise((resolve, reject)=> {
+          const storage = getStorage()
+          const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+          const storageRef = ref(storage, 'images/' + fileName)
+          const uploadTask = uploadBytesResumable(storageRef, image) //pass in the file, not the name ref. 
+
+          uploadTask.on('state_changed', 
+            (snapshot) => {
+              const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+              console.log('Upload is ' + progress + '% done');
+              switch (snapshot.state) {
+                case 'paused':
+                  console.log('Upload is paused');
+                  break;
+                case 'running':
+                  console.log('Upload is running');
+                  break;
+              }
+            }, 
+            (error) => {
+              reject(error)
+            }, 
+            () => {
+              getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                resolve(downloadURL)
+              });
+            }
+          );
+        })
+      }
+
+      
+    const imageUrls =  await Promise.all(
+      [...images].map((image) => {
+       toast.success(`File ${image.name} uploaded`)
+       console.log(image)
+       return  storeImage(image)
+      })
+    ).catch(() => {
+      setLoading(false)
+      toast.error('Could not upload images')
+      return
+    })
+    const formDataCopy = {
+      ...formData,
+      imageUrls,
+      geolocation,
+      timestamp: serverTimestamp()
     }
+
+    delete formDataCopy.images //delete image object from formData, not needed in database
+    delete formDataCopy.address//address needs to be refreshed every submission.  
+    location && (formDataCopy.location = location)
+    !formDataCopy.offer && delete formDataCopy.discountedPrice
+
+    const docRef = await addDoc(collection(db, 'listings'), formDataCopy) // addsDoc to DB, Listings collection
+    setLoading(false)
+    toast.success('Listing Created')
+    navigate(`/category:${formDataCopy.type}/${docRef.id}`)
+   
+  }
 
     const onMutate = (e) => {
         let boolean = null;
@@ -127,7 +196,7 @@ function CreateListing() {
                 <label className='formLabel'>Sell/Rent</label>
                 <div className="formButtons">
                     <button type = 'button' className = {type === 'sell' ? 'formButtonActive' : 'formButton'} id = 'type'
-                    value = 'sell' onClick={onMutate}>Sell</button>
+                    value = 'sale' onClick={onMutate}>Sale</button>
                      <button type = 'button' className = {type === 'rent' ? 'formButtonActive' : 'formButton'} id = 'type'
                     value = 'rent' onClick={onMutate}>Rent</button>
                 </div>
